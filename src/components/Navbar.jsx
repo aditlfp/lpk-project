fb3312d4e972e66ce7e0bce9f28f4b3c5ee4f100
syafi-M <syafimq00@gmail.com>
2025-08-02 19:07:41 +0700

    feat: enhance Navbar component with mobile navigation and layout improvements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,47 @@
+import { useState } from "react";
 import logo from "../assets/logo.png";
+import { LuX } from "react-icons/lu";
+import { RiMenu5Fill } from "react-icons/ri";
 
 function Navbar() {
+  const [mobileNav, setMobileNav] = useState(false);
+
   return (
     <div className="navbar bg-base-100 shadow-sm fixed mb-10 z-50 top-0 left-0">
-      <div className="flex justify-center w-screen">
-        <div className="flex justify-between items-center gap-x-10 p-3 sm:p-3">
-          <>
+      <div className="flex justify-center items-center w-screen">
+        <div className="grid grid-cols-5 justify-between items-center gap-x-4 md:gap-x-10 p-3 sm:p-3">
+          <div className="col-span-2">
             <img
               src={logo}
               alt=""
-              srcset=""
-              className="w-[25%] h-[80%] sm:w-[75%]"
+              srcSet=""
+              className="w-[100%] h-auto sm:w-[75%]"
             />
-          </>
+          </div>
 
           {/* Nav Mobile */}
-          <div className="dropdown">
+          <div
+            onClick={() => setMobileNav(!mobileNav)}
+            className="dropdown col-span-1 flex justify-center"
+          >
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                {" "}
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />{" "}
-              </svg>
+              {mobileNav ? (
+                <LuX className="w-6 h-6" />
+              ) : (
+                <RiMenu5Fill className="w-6 h-6" />
+              )}
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box rounded-t-none z-0 mt-[45pt] sm:mt-[22.5pt] w-[80vw] p-2 drop-shadow-md"
             >
               <li>
-                <div class="dropdown dropdown-hover uppercase font-bold w-full">
+                <div className="dropdown dropdown-hover uppercase font-bold w-full">
                   <label
                     tabIndex="0"
                     className="bg-transparent hover:text-amber-500 group flex items-center gap-2"
                   >
-                    MediaSosial
+                    Media Sosial
                     <svg
                       className="w-4 h-4 transition-transform duration-300 group-hover:rotate-180"
                       fill="none"
@@ -130,16 +128,16 @@ function Navbar() {
                     Sign In
                   </label>
                 </div>
-                <div className="navbar-end ml-16">
-                  <a className="btn btn-warning text-white font-bold transition-transform duration-300 hover:-translate-y-4 hover:bg-blue-600 hover:border-0 shadow-none">
+                <div className="navbar-end mx-auto">
+                  <a className="btn btn-warning text-white font-bold transition-transform duration-300 hover:scale-105 hover:bg-blue-600 hover:border-0 shadow-none">
                     Daftar Sekarang
                   </a>
                 </div>
               </li>
             </ul>
           </div>
-          <div className="lg:hidden">
-            <a className="btn btn-sm btn-warning text-white font-bold transition-transform duration-300 hover:-translate-y-4 hover:bg-blue-600 hover:border-0 shadow-none">
+          <div className="lg:hidden col-span-2 flex justify-end mr-1">
+            <a className="btn btn-sm btn-warning text-xs whitespace-nowrap text-white font-bold transition-transform duration-300 hover:scale-105 hover:bg-blue-600 hover:border-0 shadow-none">
               Daftar Sekarang
             </a>
           </div>
@@ -148,7 +146,7 @@ function Navbar() {
 
         {/* Nav Pc */}
         <div className="navbar-center hidden lg:flex gap-x-9">
-          <div class="dropdown dropdown-hover uppercase font-bold w-full">
+          <div className="dropdown dropdown-hover uppercase font-bold w-full">
             <label
               tabIndex="0"
               className="bg-transparent hover:text-amber-500 group flex items-center gap-2"
@@ -228,8 +226,8 @@ function Navbar() {
               Sign In
             </label>
           </div>
-          <div className="navbar-end ml-16">
-            <a className="btn btn-warning text-white font-bold transition-transform duration-300 hover:-translate-y-4 hover:bg-blue-600 hover:border-0 shadow-none">
+          <div className="navbar-end ml-14 mr-4">
+            <a className="btn btn-warning text-white font-bold transition-transform duration-300 hover:scale-110 hover:bg-blue-600 hover:border-0 shadow-none">
               Daftar Sekarang
             </a>
           </div>
